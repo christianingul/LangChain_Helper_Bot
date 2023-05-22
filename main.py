8c3967d3a458bf36ddf3bfbbe5dcec442bfd86f1
Christian Magnus Bjoerk Ingul <106429512+christianingul@users.noreply.github.com>
2023-05-21 18:15:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,47 @@
-from typing import Set
+import os
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+import pinecone
 import streamlit as st
-from core import run_llm
-from streamlit_chat import message
+from langchain.vectorstores import Pinecone
+from typing import Any, List
 
-st.header("LangChain Documentation Helper Bot")
 
-password_secret = st.secrets.get("PASSWORD")
 
-if password_secret is None:
-    st.error("Required secrets are missing. Please check your secrets configuration.")
-    st.stop()
 
-password = st.text_input("Enter password:", type="password")
-if password != password_secret.get("password"):
-    st.error("Reach out to cingul@usc.edu for a password")
-    st.stop()
+pinecone_secret = st.secrets.get("pinecone2")
+pinecone_env = st.secrets.get("environment2")
 
-prompt = st.text_input("Prompt", placeholder="Enter your prompt here...", key='prompt_key')
 
-if "user_prompt_history" not in st.session_state:
-    st.session_state["user_prompt_history"] = []
+pinecone_api_key = pinecone_secret.get("key")
+pinecone_environment = pinecone_env.get("key")
 
-if "chat_answers_history" not in st.session_state:
-    st.session_state["chat_answers_history"] = []
+pinecone.init(
+    api_key=pinecone_api_key,
+    environment=pinecone_environment,
+)
 
-if "chat_history" not in st.session_state:
-    st.session_state["chat_history"] = []
 
-def create_sources_string(source_urls: Set[str]) -> str:
-    if not source_urls:
-        return ""
-    sources_list = list(source_urls)
-    sources_list.sort()
-    sources_string = "sources:\n"
-    for i, source in enumerate(sources_list):
-        sources_string += f"{i+1}. {source}\n"
-    return sources_string
+def run_llm(query: str, chat_history: List[tuple[str, Any]] = []) -> Any:
+    openai_secret = st.secrets.get("openai")
+    openai_api_key = openai_secret.get("key")
 
-if prompt:
-    with st.spinner("Generating response..."):
-        generated_response = run_llm(
-            query=prompt, chat_history=st.session_state["chat_history"]
-        )
-        sources = set(
-            [doc.metadata["source"] for doc in generated_response["source_documents"]]
-        )
-        formatted_response = (
-            f"{generated_response['answer']} \n\n {create_sources_string(sources)}"
-        )
-        st.session_state["user_prompt_history"].append(prompt)
-        st.session_state["chat_answers_history"].append(formatted_response)
-        st.session_state["chat_history"].append((prompt, generated_response["answer"]))
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    docsearch = Pinecone.from_existing_index(
+        index_name="langchain-information", embedding=embeddings
+    )
+    chat = ChatOpenAI(model_name="gpt-3.5-turbo",verbose=True, temperature=0, openai_api_key=openai_api_key)
 
-        # clear the chat input
-        st.text_input("Prompt", value='', key='prompt_key')
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
+    )
+    return qa({"question": query, "chat_history": chat_history})
 
-if st.session_state["chat_answers_history"]:
-    for i, (generated_response, user_query) in enumerate(
-        zip(
-            st.session_state["chat_answers_history"],
-            st.session_state["user_prompt_history"],
-        )
-    ):
-        message(user_query, is_user=True, key=f"user_msg_{i}")
-        message(generated_response, key=f"bot_msg_{i}")
+
+#if __name__ == "__main__":
+    #result = run_llm(query="What is RetrievalQA?")
+    #print(result)
+
+
