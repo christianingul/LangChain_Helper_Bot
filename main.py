4cd9e32df058cb9d5a2f558b6af3f7f2e5d710a3
Christian Magnus Bjoerk Ingul <106429512+christianingul@users.noreply.github.com>
2023-05-21 18:13:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from typing import Set
 import streamlit as st
-from core import run_llm
-
+from backend.core import run_llm
 from streamlit_chat import message
 
-# this is the chat interface
-
-st.header("LangChain Documentation Chatbot 🦜🔗")
-
+st.header("LangChain Documentation Helper Bot")
 
 password_secret = st.secrets.get("PASSWORD")
 
@@ -20,8 +16,7 @@ if password != password_secret.get("password"):
     st.error("Reach out to cingul@usc.edu for a password")
     st.stop()
 
-prompt = st.text_input("Prompt", placeholder="Enter your prompt here...")
-
+prompt = st.text_input("Prompt", placeholder="Enter your prompt here...", key='prompt_key')
 
 if "user_prompt_history" not in st.session_state:
     st.session_state["user_prompt_history"] = []
@@ -31,7 +26,6 @@ if "chat_answers_history" not in st.session_state:
 
 if "chat_history" not in st.session_state:
     st.session_state["chat_history"] = []
-
 
 def create_sources_string(source_urls: Set[str]) -> str:
     if not source_urls:
@@ -43,7 +37,6 @@ def create_sources_string(source_urls: Set[str]) -> str:
         sources_string += f"{i+1}. {source}\n"
     return sources_string
 
-
 if prompt:
     with st.spinner("Generating response..."):
         generated_response = run_llm(
@@ -52,16 +45,16 @@ if prompt:
         sources = set(
             [doc.metadata["source"] for doc in generated_response["source_documents"]]
         )
-
         formatted_response = (
             f"{generated_response['answer']} \n\n {create_sources_string(sources)}"
         )
-
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_answers_history"].append(formatted_response)
         st.session_state["chat_history"].append((prompt, generated_response["answer"]))
 
-#ChatGPT fix
+        # clear the chat input
+        st.text_input("Prompt", value='', key='prompt_key')
+
 if st.session_state["chat_answers_history"]:
     for i, (generated_response, user_query) in enumerate(
         zip(
@@ -71,4 +64,3 @@ if st.session_state["chat_answers_history"]:
     ):
         message(user_query, is_user=True, key=f"user_msg_{i}")
         message(generated_response, key=f"bot_msg_{i}")
-
